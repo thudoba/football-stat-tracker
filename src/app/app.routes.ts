08e3f3298e11b26ff116708f0f8719aadbc3be6c
Tim Hudoba <timhudoba@gmail.com>
2026-04-22 13:53:10 -0500

    app.routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { GameList } from './pages/game-list/game-list';
+import { TeamList } from './pages/team-list/team-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Dashboard}, 
+    {path: 'games', component: GameList},
+    {path: 'teams', component: TeamList}
+];
