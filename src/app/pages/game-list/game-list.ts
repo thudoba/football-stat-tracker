797475e0f2085a429e10482d67ca552c0c4a6028
Tim Hudoba <timhudoba@gmail.com>
2026-04-22 13:46:25 -0500

    Angular and bootstrap setup

---
+++ src/app/pages/game-list/game-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-list',
+  imports: [],
+  templateUrl: './game-list.html',
+  styleUrl: './game-list.css',
+})
+export class GameList {}
---
+++ src/app/pages/team-list/team-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-list',
+  imports: [],
+  templateUrl: './team-list.html',
+  styleUrl: './team-list.css',
+})
+export class TeamList {}
