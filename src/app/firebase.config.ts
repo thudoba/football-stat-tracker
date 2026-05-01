c87b0b65dc97eb96c7d104b5d3e22c055b0074cb
ArrenM <arren.meuchel@gmail.com>
2026-05-01 10:48:46 -0500

    added firebase config

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBE3iTW636Y5VxoYOam7KFk7NNYN-ldkS8',
+  authDomain: 'football-stat-tracker-1608f.firebaseapp.com',
+  projectId: 'football-stat-tracker-1608f',
+  storageBucket: 'football-stat-tracker-1608f.firebasestorage.app',
+  messagingSenderId: '552441049382',
+  appId: '1:552441049382:web:c174d8b96dc753a48611e7',
+  measurementId: 'G-DTB9PQ7PNC',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
